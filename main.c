9422865c540aa416d996263ff8aabb32ab96ad45
cameronmoses-source <cameron.moses@torontomu.ca>
2026-03-25 12:52:38 -0400

    Add function prototypes and file handling in main.c

--- main.c
+++ main.c
@@ -1,4 +1,26 @@
+#include <stdio.h>
 
+// function parking lot:
+
+void swap(double *a, double *b);  //function that swaps two values
+
+void bubblesort(double arr[]);  // function that sorts an array from lowest to highest 
+
+double findMedian(double arr[], int start, int end);   // finds the median of an array
+
+void findQuartiles(double arr[], int size, double *Q1, double *Q2, double *Q3); // Calculates the quartiles 
+
+double mean(double arr[], int size);  // computes the average of all values in the array
+
+
+int main(void) {
+    
+    fopen("all_year_glsea_avg_c_C.txt", "r");
+    
+    fscanf("all_year_glsea_avg_c_C.txt")
+
+
+      }
 
 
 
